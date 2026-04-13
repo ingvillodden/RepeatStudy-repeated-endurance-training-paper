ea9d89b5110d4ed2ff333c98ca3b4db6f1f67b00
Ingvill Urianstad Odden <ingvill.odden@inn.no>
2026-04-13 22:18:30 +0200

    revision

--- R/libs.R
+++ R/libs.R
@@ -17,9 +17,12 @@ library(flextable)
 library(ggh4x)
 library(ggpubr)
 library(cowplot)
+library(Hmisc)
 
 # Modeling
 library(lme4)
 library(emmeans)
 library(psych)
 
+# Additional 
+library(broom.mixed)
